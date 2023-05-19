4e7c326a370031808519f10a938ca8d6d203de5e
Felix Lohmeier <mail@felixlohmeier.de>
2023-05-20 01:46:25 +0200

    Änderungsstatistik mit git diff

--- scripts/main.sh
+++ scripts/main.sh
@@ -1,4 +1,4 @@
 DIR="${DIR:-example}"
 orcli import csv "${DIR}"/input/*.csv --projectName "test"
 orcli transform "test" config/main/*.json
-orcli export tsv "test" --output "${DIR}/output/test.tsv"
+orcli export tsv "test" --output "${DIR}/tmp/test.tsv"
