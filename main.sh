39a8a46b6a4bbfdb944511abd922d3602e404688
felixlohmeier <mail@felixlohmeier.de>
2023-05-19 08:18:58 +0000

    orcli scripts in ordner scripts verschoben Teil 2

--- main.sh
+++
@@ -1,4 +0,0 @@
-DIR="${DIR:-example}"
-orcli import csv "${DIR}"/input/*.csv --projectName "test"
-orcli transform "test" config/main/*.json
-orcli export tsv "test" --output "${DIR}/output/test.tsv"
