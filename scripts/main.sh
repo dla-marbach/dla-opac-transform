e8b9abefad7593585aa00e8a5f0a496e9a534244
Felix Lohmeier <mail@felixlohmeier.de>
2025-09-26 10:06:52 +0000

    Dateien in dla-opac-gnd-enrichment umbenannt

--- scripts/main.sh
+++ scripts/main.sh
@@ -16,7 +16,7 @@ for f in "${DIR}"/tmp/config/mappings/* ; do
 done
 
 # Enrichment Cache
-files=(ak-gnd-wikidata.tsv ks-gnd-wikidata.tsv pe-gnd-wikidata.tsv)
+files=(lobid-wikidata-ak.tsv lobid-wikidata-ks.tsv lobid-wikidata-pe.tsv)
 url="https://github.com/opencultureconsulting/dla-opac-gnd-enrichment/raw/refs/heads/main/output/"
 for f in "${files[@]}"; do
     orcli import tsv "${url}${f}" --projectName "${f%.tsv}" --columnNames "key,value"
