4c181f3cbf66c52a59aec7a8c75c71b754d39cf8
Felix Lohmeier <mail@felixlohmeier.de>
2026-04-13 14:25:45 +0000

    9000 Umstellung Systematik und Tektonik erste Korrekturen

--- scripts/systematik.sh
+++ scripts/systematik.sh
@@ -18,64 +18,78 @@ orcli transform "se" "${DIR}/tmp/config/systematik/systematik-00-se-05.json"
 # Mappingtabellen erstellen
 for i in autoren bibliographie fach provenienz; do
     orcli export template "se" "${DIR}/tmp/config/systematik/systematik-00-${i}.txt" --separator "" > "${DIR}/tmp/tmp-mapping-${i}.csv" &
+    pids+=($!)
 done; for i in ${!pids[@]}; do wait ${pids[i]}; unset pids[$i]; done
 
 for i in autoren bibliographie fach provenienz; do
     orcli import csv "${DIR}/tmp/tmp-mapping-${i}.csv" --projectName "mapping-${i}" &
+    pids+=($!)
 done; for i in ${!pids[@]}; do wait ${pids[i]}; unset pids[$i]; done
 
 # Projekte erstellen aus Init-Dateien
 for i in autoren bibliographie fach provenienz; do
     orcli import tsv "${DIR}/tmp/config/systematik/init-systematik-${i}.tsv" --projectName "systematik-${i}" &
+    pids+=($!)
 done; for i in ${!pids[@]}; do wait ${pids[i]}; unset pids[$i]; done
 
 # Systematiken aufbauen
 for i in autoren bibliographie fach provenienz; do
     orcli transform "systematik-${i}" "${DIR}/tmp/config/systematik/systematik-01-${i}.json" &
+    pids+=($!)
 done; for i in ${!pids[@]}; do wait ${pids[i]}; unset pids[$i]; done
 
 # ak, ks, pe, th in OpenRefine laden
 for set in ak ks pe th; do
     orcli import csv "${DIR}"/output/${set}*.csv --projectName "${set}" &
+    pids+=($!)
 done; for i in ${!pids[@]}; do wait ${pids[i]}; unset pids[$i]; done
 
 # Systematiken anreichern
 # Spalten erstellen für Anreicherung
 for i in autoren bibliographie fach provenienz; do
     orcli transform "systematik-${i}" "${DIR}/tmp/config/systematik/systematik-02.json" &
+    pids+=($!)
 done; for i in ${!pids[@]}; do wait ${pids[i]}; unset pids[$i]; done
 # Anreicherung ak
 for i in autoren bibliographie fach provenienz; do
     orcli transform "systematik-${i}" "${DIR}/tmp/config/systematik/systematik-02-ak.json" &
+    pids+=($!)
 done; for i in ${!pids[@]}; do wait ${pids[i]}; unset pids[$i]; done
 # Anreicherung ks
 for i in autoren bibliographie fach provenienz; do
     orcli transform "systematik-${i}" "${DIR}/tmp/config/systematik/systematik-02-ks.json" &
+    pids+=($!)
 done; for i in ${!pids[@]}; do wait ${pids[i]}; unset pids[$i]; done
 # Anreicherung pe
 for i in autoren bibliographie fach provenienz; do
     orcli transform "systematik-${i}" "${DIR}/tmp/config/systematik/systematik-02-pe.json" &
+    pids+=($!)
 done; for i in ${!pids[@]}; do wait ${pids[i]}; unset pids[$i]; done
 # Anreicherung sy
 for i in autoren bibliographie fach provenienz; do
     orcli transform "systematik-${i}" "${DIR}/tmp/config/systematik/systematik-02-sy.json" &
+    pids+=($!)
 done; for i in ${!pids[@]}; do wait ${pids[i]}; unset pids[$i]; done
 # Anreicherung th
 for i in autoren bibliographie fach provenienz; do
     orcli transform "systematik-${i}" "${DIR}/tmp/config/systematik/systematik-02-th.json" &
+    pids+=($!)
 done; for i in ${!pids[@]}; do wait ${pids[i]}; unset pids[$i]; done
 # 1. Glied der Kette für Suche anreichern
 for i in autoren bibliographie fach provenienz; do
     orcli transform "systematik-${i}" "${DIR}/tmp/config/systematik/systematik-03-${i}.json" &
+    pids+=($!)
 done; for i in ${!pids[@]}; do wait ${pids[i]}; unset pids[$i]; done
 # Spalten Sortieren
 for i in autoren bibliographie fach provenienz; do
     orcli transform "systematik-${i}" "${DIR}/tmp/config/systematik/systematik-04.json" &
+    pids+=($!)
 done; for i in ${!pids[@]}; do wait ${pids[i]}; unset pids[$i]; done
 
 # Zusammenführung Systematiken
 for i in autoren bibliographie fach provenienz; do
     orcli export tsv "systematik-${i}" --output "${DIR}/tmp/tmp-systematik-${i}.tsv" &
+    pids+=($!)
 done; for i in ${!pids[@]}; do wait ${pids[i]}; unset pids[$i]; done
 zip "${DIR}/tmp/tmp-systematik.zip" "${DIR}/tmp/config/systematik/init-systematik.tsv" "${DIR}/tmp/tmp-systematik-autoren.tsv" "${DIR}/tmp/tmp-systematik-bibliographie.tsv" "${DIR}/tmp/tmp-systematik-fach.tsv" "${DIR}/tmp/tmp-systematik-provenienz.tsv"
 orcli import tsv "${DIR}/tmp/tmp-systematik.zip" --projectName "systematik"
